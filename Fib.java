8cbb7396e6619a93d162969d44d2860143184634
KennethTillit <112890518+KennethTillit@users.noreply.github.com>
2023-01-19 14:08:18 -0500

    Add files via upload

--- Fib.java
+++ Fib.java
@@ -3,8 +3,15 @@ package ictModule1;
 import java.util.Scanner;
 
 public class Fib {
-	public static int element = 0;
 	public static void main (String args[] ) {
+		/**
+		 * @author Kenny
+		 * This sequence opens a scanner, then asks the user for a number.
+		 * The number is then taken and uses the fibonacci method below.
+		 * Once the fibonacci method is complete, the findings are printed
+		 * out, including the original number, and the number representing
+		 * it in the fibonacci sequence.
+		 */
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("Input a Number:");
 		int num = scnr.nextInt();
@@ -14,6 +21,15 @@ public class Fib {
 	}
 	
 	public static int fibonacci(int n) {
+		/**
+		 * @author Kenny
+		 * This sequence is used to determine what number correlates to
+		 * the number provided based on the fibonacci sequence.
+		 * It takes the number given from the console, and return's n.
+		 * It uses recursive means to determine the number.
+		 * @param n
+		 * @return n
+		 */
 		if (n <= 1) {
 			return n;
 		}
